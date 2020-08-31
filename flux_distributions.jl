77fe0cd49c08830796c060d02e280da4104a3707
Phil <ps12e@my.fsu.edu>
2020-08-31 16:30:57 -0400

    New timescale dist

--- flux_distributions.jl
+++ flux_distributions.jl
@@ -30,30 +30,22 @@ A = Float64.(Matrix(CSV.read("karate.csv";header=false)))
 @time Phi = flow_matrix(A)
 
 @time lambda, v = eigen(Phi)
-flux = zeros(34,5)
+flux = zeros(34,3)
 flux[:,2] = real(v[:,1])
 
-Phi = flow_matrix(A,b=0.001)
+Phi = flow_matrix(A,b=0.015)
 lambda, v = eigen(Phi)
-flux[:,1] = real(v[:,1])
+flux[:,1] = -real(v[:,1])
 
-Phi = flow_matrix(A,b=0.01)
+Phi = flow_matrix(A,b=0.15)
 lambda, v = eigen(Phi)
 flux[:,2] = -real(v[:,1])
 
-Phi = flow_matrix(A,b=0.1)
+Phi = flow_matrix(A,b=1.5)
 lambda, v = eigen(Phi)
 flux[:,3] = -real(v[:,1])
 
-Phi = flow_matrix(A,b=1)
-lambda, v = eigen(Phi)
-flux[:,4] = -real(v[:,2])
-
-Phi = flow_matrix(A,b=10)
-lambda, v = eigen(Phi)
-flux[:,5] = -real(v[:,2])
-
-writedlm("fluxcent_karate.csv",flux,',')
+writedlm("fluxcent_karate3.csv",flux,',')
 
 A = Float64.(Matrix(CSV.read("celegans.csv";header=false)))
 
