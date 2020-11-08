00905d5caa06d0f1df03ba64e5156fc73eede97d
Phil <ps12e@my.fsu.edu>
2020-11-08 03:34:18 -0500

    Plotting various things

--- state-selection.jl
+++ state-selection.jl
@@ -78,39 +78,68 @@ W,err = gramian(A,b1)
 @show eigen(W)
 @show rank(W)
 
-M = pinv(gramian(A,testb,0.,15.))
 
 using Plots, PlotThemes
 
 eta = 1.
 xfin = optimal_mean_state(testb,A,eta,x0,t1=15.)
-
+M = pinv(gramian(A,testb,0.,15.))
 plot(t -> u(t,A,testb,x0,M,tf=15.,xf = xfin)[1],0.,15.)
 plot!(t -> u(t,A,testb,x0,M,tf=15.,xf = xfin)[2],0.,15.)
 
-plt = plot()
+plt = plot(xlabel="t", ylabel="State",legendfontsize=14,tickfontsize=14,guidefontsize=14)
 for j=1:4
 	plot!(plt, i -> trajectory(A,testb,i,x0,M,xfi=xfin)[j], 0, 1.,label="")
 end
-plot!(plt, i -> sum(trajectory(A,testb,i,x0,M,xfi=xfin))/4, 0, 1.,label="Average",linecolor=:black, linestyle=:dash)
+plot!(plt, i -> sum(trajectory(A,testb,i,x0,M,xfi=xfin))/4, 0, 1.,label="",linecolor=:black, linestyle=:dash, linewidth=2)
+plot!(plt, i -> 1, label = "", linecolor=:black, linewidth=2, linestyle=:solid)
 plot!()
 
 xfin2 = optimal_mean_state(testb,A,eta,x0,t1=15.)
-plt2 = plot()
+plt2 = plot(xlabel="t", ylabel="State",legendfontsize=14,tickfontsize=14,guidefontsize=14)
 for j=1:4
 	plot!(plt2, i -> trajectory(A,testb,i,x0,M,t1=15.,xfi=xfin2)[j], 0, 15.,label="")
 end
-plot!(plt2, i -> sum(trajectory(A,testb,i,x0,M,t1=15.,xfi=xfin2))/4, 0, 15.,label="Average",linecolor=:black, linestyle=:dash)
-plot!()
+plot!(plt2, i -> sum(trajectory(A,testb,i,x0,M,t1=15.,xfi=xfin2))/4, 0, 15.,label="",linecolor=:black, linestyle=:dashdot, linewidth=2)
+plot!(plt2, i -> 1, label = "", linecolor=:black, linestyle=:solid, linewidth=2)
+plot!(plt2, [15,15],[-6,7.5],label = "",linecolor=:black, linestyle = :dash)
 savefig(plt2,"controlled.pdf")
 
-plt3 = plot()
+plt3 = plot(xlabel="t", ylabel="State",legendfontsize=14,tickfontsize=14,guidefontsize=14,legend=:bottomleft)
 for j=1:4
 	plot!(plt3, i -> trajectory(A,zeros(4,2),i,x0,M,xfi=xfin2)[j], 0, 15.,label="")
 end
-plot!(plt3, i -> sum(trajectory(A,zeros(4,2),i,x0,M,xfi=xfin2))/4, 0, 15.,label="Average",linecolor=:black, linestyle=:dash)
+plot!(plt3, i -> sum(trajectory(A,zeros(4,2),i,x0,M,xfi=xfin2))/4, 0, 15.,label="Average state",linecolor=:black, linestyle=:dashdot, linewidth=2)
+plot!(plt3, i -> 1, label = "Desired threshold", linecolor=:black, linewidth=2, linestyle=:solid)
+plot!(plt3, [15,15],[-5,5],label = "",linecolor=:black, linestyle = :dash)
 plot!()
 savefig(plt3,"autonomous.pdf")
+
+using Random
+Random.seed!(123)
+randb = sphere_projection(rand(4,2),2)
+xfin3 = optimal_mean_state(randb,A,eta,x0,t1=15.)
+M2 = pinv(gramian(A,randb,0.,15.))
+plt3 = plot(xlabel="t", ylabel="State",legendfontsize=14,tickfontsize=14,guidefontsize=14,legend=:bottomleft)
+for j=1:4
+	plot!(plt3, i -> trajectory(A,randb,i,x0,M2,t1=15.,xfi=xfin3)[j], 0, 15.,label="")
+end
+plot!(plt3, i -> sum(trajectory(A,randb,i,x0,M2,t1=15.,xfi=xfin3))/4, 0, 15.,label="", linewidth=2, linecolor=:black, linestyle=:dashdot)
+plot!(plt3, i -> 1, label = "", linecolor=:black, linewidth=2, linestyle=:solid)
+plot!(plt3, [15,15],[-4,7.5],label = "",linecolor=:black, linestyle = :dash)
+savefig(plt3,"randb.pdf")
+
+plt4 = plot(t -> u(t,A,randb,x0,M,tf=15.,xf = xfin3)[1],0.,15.,label="RAM 1",xlabel="t",ylabel="Input Signal",legendfontsize=14,tickfontsize=14,guidefontsize=14,linewidth=2,legend=:topleft,linecolor=:black,linestyle=:dash)
+plot!(plt4,t -> u(t,A,randb,x0,M,tf=15.,xf = xfin3)[2],0.,15.,label="RAM 2",linestyle=:dashdot,linewidth=2,linecolor=:black)
+plot!(plt4,t -> u(t,A,testb,x0,M,tf=15.,xf = xfin2)[1],0.,15.,label="Flux 1 & 2",linestyle=:solid,linewidth=2,linecolor=:black)
+savefig(plt4,"inputs.pdf")
+
+
+ninp_rand(t) = (u(t,A,randb,x0,M,tf=15.,xf = xfin3)[1])^2 + (u(t,A,randb,x0,M,tf=15.,xf = xfin3)[2])^2
+plt5 = plot(t -> quadgk(a -> ninp_rand(a),0.,t)[1],0.,15.,label="RAM",linestyle=:dash,legend=:topleft,linecolor=:black,linewidth=2,ylabel="Cumulative Input Energy",xlabel="t")
+ninp_opt(t) = (u(t,A,testb,x0,M,tf=15.,xf = xfin2)[1])^2 + (u(t,A,testb,x0,M,tf=15.,xf = xfin2)[2])^2
+plot!(plt5,t -> quadgk(a -> ninp_opt(a),0.,t)[1],0.,15.,label="Flux",linestyle=:solid,linecolor=:black,linewidth=2)
+savefig(plt5,"energies.pdf")
 
 @time b2 = pgm_max_sync(A,b1,1.)
 
